794d7c6df4d1ee953b6b06c59782267225d021d6
danielsansp <daniel1008ospina@gmail.com>
2021-12-21 16:18:43 -0500

    subiendo archivos

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class App extends React.Component{
+
+
+    render(){
+        return(
+            <div>
+
+               <form action="./mundo">
+                   <label htmlFor="mundo">
+                       <h1>Hola mundo</h1>
+                       <p>{this.props.name}</p>
+                       <p>lorem500000dc dchbdshcbdcdcvdfbvsdfbvfnfdbvhfbvfhbv</p>
+                   </label>
+               </form>
+            </div>
+        )
+    }
+}
+
+
+
+
+    export default App;
